9cb46d7de55a0fc9627ec9aaad93fcc892b524f1
kristervv <89388846+kristervv@users.noreply.github.com>
2021-10-18 13:40:05 +0200

    La til kode i sok.js

---
+++ sok.js
@@ -0,0 +1,16 @@
+
+//https://www.geeksforgeeks.org/search-bar-using-html-css-and-javascript/
+function search(tagNavn) {
+    let input = document.getElementById('myInput').value
+    input=input.toLowerCase();
+    let x = document.getElementsByClassName(tagNavn);
+
+    for (i = 0; i < x.length; i++) {
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
+            x[i].style.display="none";
+        }
+        else {
+            x[i].style.display="list-item";
+        }
+    }
+}
